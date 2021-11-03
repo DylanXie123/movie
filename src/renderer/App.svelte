da8c4650e07c77bcdfd02e94cb110e6c2394546f
DylanXie123 <dylanxie123@outlook.com>
2021-11-03 21:15:55 +0800

    style(library): add bootstrap as ui library

--- src/renderer/App.svelte
+++ src/renderer/App.svelte
@@ -1,75 +1,10 @@
 <script lang="ts">
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import "bootstrap-icons/font/bootstrap-icons.css";
   import { Router, Link, Route } from "svelte-routing";
-
   import Home from "./home/index.svelte";
   import Detail from "./detail/index.svelte";
-
-  const create = () =>
-    window.dbAPI
-      .create({
-        fullPath: "https://svelte-routing.firebaseio.com/",
-        title: "new item",
-        tmdbID: 0,
-        imdbID: 0,
-        posterURL: "https://via.placeholder.com/300x450",
-        backgroundURL: "https://via.placeholder.com/300x450",
-        overview: "",
-        language: "",
-        releaseDate: '0',
-        tmdbRating: 0,
-        imdbRating: 0,
-      })
-      .then(console.log);
-
-  const update = () =>
-    window.dbAPI
-      .update({
-        fullPath: "https://svelte-routing.firebaseio.com/",
-        title: "update item",
-        tmdbID: 0,
-        imdbID: 0,
-        posterURL: "https://via.placeholder.com/300x450",
-        backgroundURL: "https://via.placeholder.com/300x450",
-        overview: "",
-        language: "",
-        releaseDate: '0',
-        tmdbRating: 0,
-        imdbRating: 0,
-      })
-      .then(console.log);
-
-  const retrieveAll = () => window.dbAPI.retrieveAll().then(console.log);
-
-  const retrieve = () =>
-    window.dbAPI
-      .retrieve("https://svelte-routing.firebaseio.com/")
-      .then(console.log);
-
-  const deleteItem = () =>
-    window.dbAPI
-      .delete("https://svelte-routing.firebaseio.com/")
-      .then(console.log);
-
-  const readFileNods = () => {
-    const a = window.fsAPI.initFileNodes(
-      "D:\\OneDrive - stu.xjtu.edu.cn\\Downloads"
-    );
-    console.log(a);
-    window.fsAPI.addLitsener(
-      "D:\\OneDrive - stu.xjtu.edu.cn\\Downloads",
-      (fileName) => {
-        console.log(fileName);
-      }
-    );
-  };
 </script>
-
-<button on:click={create}>Create</button>
-<button on:click={update}>Update</button>
-<button on:click={retrieve}>Retrieve</button>
-<button on:click={retrieveAll}>RetrieveAll</button>
-<button on:click={deleteItem}>Delete</button>
-<button on:click={readFileNods}>Test</button>
 
 <Router>
   <nav>
--- src/renderer/home/index.svelte
+++ src/renderer/home/index.svelte
@@ -2,11 +2,28 @@
   import fileNodeStore from "../store/fileNodeStore";
 </script>
 
-<p>Home Page</p>
-
-<div class="container">
+<div class="grid-container">
   {#each $fileNodeStore as node}
-    <img src={node.posterURL} alt={node.parsed.name} />
+    <div class="d-grid gap-2">
+      <img src={node.posterURL} alt={node.parsed.name} />
+      <form class="row g-2">
+        <div class="col-auto">
+          <label for="staticEmail2" class="visually-hidden">Email</label>
+          <input
+            type="text"
+            readonly
+            class="form-control-plaintext"
+            id="staticEmail2"
+            value="email@example.com"
+          />
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary mb-3"
+            >Confirm identity</button
+          >
+        </div>
+      </form>
+    </div>
   {/each}
 </div>
 
@@ -14,7 +31,7 @@
   img {
     max-width: 100%;
   }
-  .container {
+  .grid-container {
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     gap: 20px;
