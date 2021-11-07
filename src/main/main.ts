8d88e2d78412fd51f04de46fc53f81704fe38774
Dylan Xie <dylanxie123@outlook.com>
2021-11-07 12:44:47 +0800

    refactor(route): use svelte-spa-route
    new package use hash route, which is easier to use in SPA

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,8 +3,8 @@ import Store from 'electron-store';
 import type { DBNode, UpdateType } from '../fileNode';
 import { closeDB, create, deleteDB, initDatabase, retrieve, retrieveAll, update } from './database';
 
-declare const INDEX_WEBPACK_ENTRY: string;
-declare const INDEX_PRELOAD_WEBPACK_ENTRY: string;
+declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
+declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -18,12 +18,12 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: INDEX_PRELOAD_WEBPACK_ENTRY,
+      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     }
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(INDEX_WEBPACK_ENTRY);
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
--- src/renderer/App.svelte
+++ src/renderer/App.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   import "bootstrap/dist/css/bootstrap.min.css";
   import "bootstrap-icons/font/bootstrap-icons.css";
-  import { Router, Link, Route } from "svelte-routing";
+  import Router, { link } from "svelte-spa-router";
   import Home from "./home/index.svelte";
   import Detail from "./detail/index.svelte";
+
+  const routes = {
+    "/": Home,
+    "/detail/:id": Detail,
+  };
 </script>
 
-<Router>
-  <nav>
-    <Link to="index">Home</Link>
-    <Link to="detail">Detail</Link>
-  </nav>
-  <Route path="index" component={Home} />
-  <Route path="detail" component={Detail} />
-</Router>
+<nav>
+  <a href="/" use:link>Home</a>
+  <a href="/detail/1" use:link>Detail</a>
+</nav>
+<Router {routes} />
